54b7e52581cda6b6c49c04d0c553624a89ea7623
RajrupDasid <rajrupofficialid@protonmail.com>
2023-12-28 17:56:17 +0530

    full design complete
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,28 @@
 import React from "react";
+import "../assets/css/footer.css";
 
 const Footer: React.FC = () => {
   return (
     <>
-      <div>footer</div>
+      <footer className="footer">
+        <div className="footer-content">
+          <div className="footer-column">
+            <h4>About Us</h4>
+            <p>Our Mission</p>
+            <p>We're Hiring!</p>
+          </div>
+          <div className="footer-column">
+            <h4>Resources</h4>
+            <p>Tutorials</p>
+            <p>Brand Assets</p>
+          </div>
+          <div className="footer-column">
+            <h4>Contact Us</h4>
+            <p>321-555-5555</p>
+            <p>info@company.com</p>
+          </div>
+        </div>
+      </footer>
     </>
   );
 };
