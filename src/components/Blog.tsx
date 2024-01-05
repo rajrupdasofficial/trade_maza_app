35cc0b95bdd14ee3ad7534a5585b79f61b8e3074
Rajrup Das <rajrupofficialid@protonmail.com>
2024-01-05 12:45:35 +0000

    Design fix (#2)
    * responsiveness added
    
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>
    
    * responsiveness added and design fixed
    
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>
    
    ---------
    
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>

---
+++ src/components/Blog.tsx
@@ -0,0 +1,250 @@
+import Navbar from "./Nav";
+import card2 from "../assets/images/card2.webp";
+import "../assets/css/card.css";
+const Blog = () => {
+  return (
+    <>
+      <Navbar />
+      <div>
+        <div>
+          <div className="card-container">
+            <div className="card">
+              <h3>Free Webinars</h3>
+              <img src={card2} alt="Free Webinars" />
+
+              <p>Access to All Webinars</p>
+              <p>Free Technical Analysis Webinar</p>
+              <p>Free Advanced Courses Webinar</p>
+              <p>Free Price Action Trading Webinar</p>
+              <p>Free Fundamental Analysis Webinar</p>
+              <button>Read More</button>
+            </div>
+            <div className="card">
+              <h3>Free Webinars</h3>
+              <img src={card2} alt="Free Webinars" />
+
+              <p>Access to All Webinars</p>
+              <p>Free Technical Analysis Webinar</p>
+              <p>Free Advanced Courses Webinar</p>
+              <p>Free Price Action Trading Webinar</p>
+              <p>Free Fundamental Analysis Webinar</p>
+              <button>Read More</button>
+            </div>
+            <div className="card">
+              <h3>Free Webinars</h3>
+              <img src={card2} alt="Free Webinars" />
+
+              <p>Access to All Webinars</p>
+              <p>Free Technical Analysis Webinar</p>
+              <p>Free Advanced Courses Webinar</p>
+              <p>Free Price Action Trading Webinar</p>
+              <p>Free Fundamental Analysis Webinar</p>
+              <button>Read More</button>
+            </div>
+            <div className="card">
+              <h3>Free Webinars</h3>
+              <img src={card2} alt="Free Webinars" />
+
+              <p>Access to All Webinars</p>
+              <p>Free Technical Analysis Webinar</p>
+              <p>Free Advanced Courses Webinar</p>
+              <p>Free Price Action Trading Webinar</p>
+              <p>Free Fundamental Analysis Webinar</p>
+              <button>Read More</button>
+            </div>
+            <div className="card">
+              <h3>Free Webinars</h3>
+              <img src={card2} alt="Free Webinars" />
+
+              <p>Access to All Webinars</p>
+              <p>Free Technical Analysis Webinar</p>
+              <p>Free Advanced Courses Webinar</p>
+              <p>Free Price Action Trading Webinar</p>
+              <p>Free Fundamental Analysis Webinar</p>
+              <button>Read More</button>
+            </div>
+            <div className="card">
+              <h3>Free Webinars</h3>
+              <img src={card2} alt="Free Webinars" />
+
+              <p>Access to All Webinars</p>
+              <p>Free Technical Analysis Webinar</p>
+              <p>Free Advanced Courses Webinar</p>
+              <p>Free Price Action Trading Webinar</p>
+              <p>Free Fundamental Analysis Webinar</p>
+              <button>Read More</button>
+            </div>
+            <div className="card">
+              <h3>Free Webinars</h3>
+              <img src={card2} alt="Free Webinars" />
+
+              <p>Access to All Webinars</p>
+              <p>Free Technical Analysis Webinar</p>
+              <p>Free Advanced Courses Webinar</p>
+              <p>Free Price Action Trading Webinar</p>
+              <p>Free Fundamental Analysis Webinar</p>
+              <button>Read More</button>
+            </div>
+            <div className="card">
+              <h3>Free Webinars</h3>
+              <img src={card2} alt="Free Webinars" />
+
+              <p>Access to All Webinars</p>
+              <p>Free Technical Analysis Webinar</p>
+              <p>Free Advanced Courses Webinar</p>
+              <p>Free Price Action Trading Webinar</p>
+              <p>Free Fundamental Analysis Webinar</p>
+              <button>Read More</button>
+            </div>
+            <div className="card">
+              <h3>Free Webinars</h3>
+              <img src={card2} alt="Free Webinars" />
+
+              <p>Access to All Webinars</p>
+              <p>Free Technical Analysis Webinar</p>
+              <p>Free Advanced Courses Webinar</p>
+              <p>Free Price Action Trading Webinar</p>
+              <p>Free Fundamental Analysis Webinar</p>
+              <button>Read More</button>
+            </div>
+            <div className="card">
+              <h3>Free Webinars</h3>
+              <img src={card2} alt="Free Webinars" />
+
+              <p>Access to All Webinars</p>
+              <p>Free Technical Analysis Webinar</p>
+              <p>Free Advanced Courses Webinar</p>
+              <p>Free Price Action Trading Webinar</p>
+              <p>Free Fundamental Analysis Webinar</p>
+              <button>Read More</button>
+            </div>
+            <div className="card">
+              <h3>Free Webinars</h3>
+              <img src={card2} alt="Free Webinars" />
+
+              <p>Access to All Webinars</p>
+              <p>Free Technical Analysis Webinar</p>
+              <p>Free Advanced Courses Webinar</p>
+              <p>Free Price Action Trading Webinar</p>
+              <p>Free Fundamental Analysis Webinar</p>
+              <button>Read More</button>
+            </div>
+            <div className="card">
+              <h3>Free Webinars</h3>
+              <img src={card2} alt="Free Webinars" />
+
+              <p>Access to All Webinars</p>
+              <p>Free Technical Analysis Webinar</p>
+              <p>Free Advanced Courses Webinar</p>
+              <p>Free Price Action Trading Webinar</p>
+              <p>Free Fundamental Analysis Webinar</p>
+              <button>Read More</button>
+            </div>
+            <div className="card">
+              <h3>Free Webinars</h3>
+              <img src={card2} alt="Free Webinars" />
+
+              <p>Access to All Webinars</p>
+              <p>Free Technical Analysis Webinar</p>
+              <p>Free Advanced Courses Webinar</p>
+              <p>Free Price Action Trading Webinar</p>
+              <p>Free Fundamental Analysis Webinar</p>
+              <button>Read More</button>
+            </div>
+            <div className="card">
+              <h3>Free Webinars</h3>
+              <img src={card2} alt="Free Webinars" />
+
+              <p>Access to All Webinars</p>
+              <p>Free Technical Analysis Webinar</p>
+              <p>Free Advanced Courses Webinar</p>
+              <p>Free Price Action Trading Webinar</p>
+              <p>Free Fundamental Analysis Webinar</p>
+              <button>Read More</button>
+            </div>
+
+            <div className="card">
+              <h3>Free Webinars</h3>
+              <img src={card2} alt="Free Webinars" />
+
+              <p>Access to All Webinars</p>
+              <p>Free Technical Analysis Webinar</p>
+              <p>Free Advanced Courses Webinar</p>
+              <p>Free Price Action Trading Webinar</p>
+              <p>Free Fundamental Analysis Webinar</p>
+              <button>Read More</button>
+            </div>
+            <div className="card">
+              <h3>Free Webinars</h3>
+              <img src={card2} alt="Free Webinars" />
+
+              <p>Access to All Webinars</p>
+              <p>Free Technical Analysis Webinar</p>
+              <p>Free Advanced Courses Webinar</p>
+              <p>Free Price Action Trading Webinar</p>
+              <p>Free Fundamental Analysis Webinar</p>
+              <button>Read More</button>
+            </div>
+            <div className="card">
+              <h3>Free Webinars</h3>
+              <img src={card2} alt="Free Webinars" />
+
+              <p>Access to All Webinars</p>
+              <p>Free Technical Analysis Webinar</p>
+              <p>Free Advanced Courses Webinar</p>
+              <p>Free Price Action Trading Webinar</p>
+              <p>Free Fundamental Analysis Webinar</p>
+              <button>Read More</button>
+            </div>
+            <div className="card">
+              <h3>Free Webinars</h3>
+              <img src={card2} alt="Free Webinars" />
+
+              <p>Access to All Webinars</p>
+              <p>Free Technical Analysis Webinar</p>
+              <p>Free Advanced Courses Webinar</p>
+              <p>Free Price Action Trading Webinar</p>
+              <p>Free Fundamental Analysis Webinar</p>
+              <button>Read More</button>
+            </div>
+            <div className="card">
+              <h3>Free Webinars</h3>
+              <img src={card2} alt="Free Webinars" />
+
+              <p>Access to All Webinars</p>
+              <p>Free Technical Analysis Webinar</p>
+              <p>Free Advanced Courses Webinar</p>
+              <p>Free Price Action Trading Webinar</p>
+              <p>Free Fundamental Analysis Webinar</p>
+              <button>Read More</button>
+            </div>
+            <div className="card">
+              <h3>Free Webinars</h3>
+              <img src={card2} alt="Free Webinars" />
+
+              <p>Access to All Webinars</p>
+              <p>Free Technical Analysis Webinar</p>
+              <p>Free Advanced Courses Webinar</p>
+              <p>Free Price Action Trading Webinar</p>
+              <p>Free Fundamental Analysis Webinar</p>
+              <button>Read More</button>
+            </div>
+            <div className="card">
+              <h3>Free Webinars</h3>
+              <img src={card2} alt="Free Webinars" />
+
+              <p>Access to All Webinars</p>
+              <p>Free Technical Analysis Webinar</p>
+              <p>Free Advanced Courses Webinar</p>
+              <p>Free Price Action Trading Webinar</p>
+              <p>Free Fundamental Analysis Webinar</p>
+              <button>Read More</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Blog;
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,29 +1,72 @@
 import React from "react";
+// import React from "react";
+// import { HiMenu } from "react-icons/hi";
+// import { SlClose } from "react-icons/sl";
 import "../assets/css/Navbar.css";
+import { Link } from "react-router-dom";
 const Navbar: React.FC = () => {
+  // const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  // const toggleMenu = () => {
+  //   setIsMenuOpen(!isMenuOpen);
+  // };
+
+  // const closeMenu = () => {
+  //   setIsMenuOpen(false);
+  // };
   return (
-    <nav className="navbar">
-      <div className="logo">Macro</div>
-      <ul className="nav-links">
-        <li>
-          <a href="#">Hero</a>
-        </li>
-        <li>
-          <a href="#">What We Do</a>
-        </li>
-        <li>
-          <a href="#">Offer You Can't</a>
-        </li>
-        <li>
-          <a href="#">Contact Us</a>
-        </li>
-        <div className="btngrp">
+    <>
+      <nav className="navbar">
+        <div className="logo">Macro</div>
+        <ul className="nav-links">
           <li>
-            <button className="signin-btn">Sign In</button>
+            <Link to="/">Hero</Link>
           </li>
-        </div>
-      </ul>
-    </nav>
+          <li>
+            <Link to="/">What We Do</Link>
+          </li>
+          <li>
+            <Link to="/">Offer You Can't</Link>
+          </li>
+          <li>
+            <Link to="/blogs">Blogs</Link>
+          </li>
+          <li>
+            <Link to="/">Contact Us</Link>
+          </li>
+
+          <div className="btngrp">
+            <li>
+              <button className="signin-btn">Sign In</button>
+            </li>
+          </div>
+        </ul>
+        {/* <a
+          href="#"
+          className={`menu ${isMenuOpen ? "active" : ""}`}
+          onClick={toggleMenu}>
+          <HiMenu />
+        </a> */}
+      </nav>
+      {/* <ul className={`nav-mobile ${isMenuOpen ? "active" : ""}`}>
+        <SlClose />
+        <li>
+          <Link to="/" onClick={closeMenu}>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="/" onClick={closeMenu}>
+            About
+          </Link>
+        </li>
+        <li>
+          <Link to="/" onClick={closeMenu}>
+            Contact
+          </Link>
+        </li>
+      </ul> */}
+    </>
   );
 };
 
