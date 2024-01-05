b10388991e2e3cd33f2c3a482563d5a072aa213d
RajrupDasid <rajrupofficialid@protonmail.com>
2024-01-05 18:08:37 +0530

    responsiveness added and design fixed
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+import Blog from "./components/Blog";
 import Home from "./components/Home";
+import "./index.css";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/blogs" element={<Blog />} />
+      </Routes>
     </>
   );
 }
